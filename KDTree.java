c2b6ca2deaf8314569a9ab760d98713f0dac3a5f
camillatan95 <43073007+camillatan95@users.noreply.github.com>
2020-01-26 15:37:53 -0500

    Update KDTree.java

--- KDTree.java
+++ KDTree.java
@@ -1,16 +1,16 @@
-package group;
+package projects;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
-class Node {
+class NodeK {
 	private double lat;
 	private double lng;
 	private int level;
-	private Node left;
-	private Node right;
+	private NodeK left;
+	private NodeK right;
 	
-	Node (double la, double lo) {
+	NodeK (double la, double lo) {
 		this.lat = la;
 		this.lng = lo;
 		left = null;
@@ -28,32 +28,35 @@ class Node {
 	int getLev(){
 		return level;
 	} 
-	Node getLeft() {
+	NodeK getLeft() {
 		return left;
 	}
-	Node getRight() {
+	NodeK getRight() {
 		return right;
 	}
-	void setLeft(Node i) {
+	void setLeft(NodeK i) {
 		left = i;
 	}
-	void setRight(Node i) {
+	void setRight(NodeK i) {
 		right = i;
 	}
 	void setData(double x, double y) {
 		lat = x;
 		lng = y;
 	}
+	public String toString() {
+		return "("+this.lat+", "+this.lng+")";
+	}
 }
 
 public class KDTree {
-	public static Node insert(Node root, double x, double y) {   
+	public static NodeK insert(NodeK root, double x, double y) {   
 		if (root == null) {
-			Node newNode = new Node(x,y);
+			NodeK newNode = new NodeK(x,y);
 			newNode.setLevel(1);    //set root as level x (1)
 			return newNode;
 		} else {
-			Node cur;
+			NodeK cur;
 			if(root.getLev()==1) {
 				if (x < root.getX()) {
 					cur = insert(root.getLeft(), x,y);
@@ -98,50 +101,51 @@ public class KDTree {
 		}
 	}
 	*/
-	
-	public static Node find(Node root, double x, double y, int r) {   //rangeSearch e.g(60,40) r=5, 55<x<65, 35<y<45
+	static LinkedList<NodeK> arr = new LinkedList<>();
+	public static void find(NodeK root, double x, double y, int r) { 
+		//rangeSearch e.g(60,40) r=5, 55<x<65, 35<y<45
 		if (root == null) {
-			return null;
-		} else if (x<=root.getX()+r && x>root.getX()-r && y<=root.getY()+r && y>root.getY()-r){
-			//now we check if the node is in the radius
-			double dist = Math.sqrt(Math.pow(Math.abs(root.getX()-x),2) + Math.pow(Math.abs(root.getY()-y), 2));
-			if(dist<=r) return root;
-		} else {
-			if(root.getLev()==1) {
-				if (root.getX()< x-r) {
-					return null;
-				} else if (x+r < root.getX()){
-					return find(root.getLeft(), x,y,r);
-				}else if(x+r >= root.getX()) {
-					if (x<=root.getX()+r && x>root.getX()-r && y<=root.getY()+r && y>root.getY()-r){
-						//check if the node is in the radius
-						double dist = Math.sqrt(Math.pow(Math.abs(root.getX()-x),2) + Math.pow(Math.abs(root.getY()-y), 2));
-						if(dist<=r) return root;
-					}
-					find(root.getRight(), x,y,r);
-					find(root.getLeft(), x,y,r);
+			return;
+		} 
+		if (root.getLev() == 1) {
+			if (root.getX()<(x-r)) {
+				find(root.getRight(), x, y, r);
+				return;
+			}else if(root.getX()>=(x+r)){
+				find(root.getLeft(), x, y, r);
+				return;
+			}else {
+				double dist = Math.sqrt(Math.pow(Math.abs(root.getX()-x),2) 
+						+ Math.pow(Math.abs(root.getY()-y), 2));
+				if(dist<=r) {
+					arr.add(root);
 				}
-			}
-			if(root.getLev()==2) {
-				if(root.getY() < y-r) {   //current node y smaller than minimum range, break
-					return null;
-				}else if (y+r < root.getY()) {   //current node y larger than max range, check left
-					find(root.getLeft(), x,y,r);
-				} else if(y+r >= root.getY()) {   //current node y same as max range, check both left and right
-					if (x<=root.getX()+r && x>root.getX()-r && y<=root.getY()+r && y>root.getY()-r){
-						//check if the node is in the radius
-						double dist = Math.sqrt(Math.pow(Math.abs(root.getX()-x),2) + Math.pow(Math.abs(root.getY()-y), 2));
-						if(dist<=r) return root;
-					}
-					find(root.getRight(), x,y,r);
-					find(root.getLeft(), x,y,r);
-				}
+				find(root.getRight(), x,y,r);
+				find(root.getLeft(), x,y,r);
+				return;
 			}
 		}
-		return null;	
+		if (root.getLev() == 2) {
+			if (root.getY()<(y-r)) {
+				find(root.getRight(), x, y, r);
+				return;
+			}else if(root.getY()>=(y+r)){
+				find(root.getLeft(), x, y, r);
+				return;
+			}else {
+				double dist = Math.sqrt(Math.pow(Math.abs(root.getX()-x),2) 
+						+ Math.pow(Math.abs(root.getY()-y), 2));
+				if(dist<=r) {
+					arr.add(root);
+				}
+				find(root.getRight(), x,y,r);
+				find(root.getLeft(), x,y,r);
+				return;
+			}
+		}
 	}
 	
-	public static Boolean efind(Node root, double x, double y) {   //exactSearch
+	public static Boolean efind(NodeK root, double x, double y) {   //exactSearch
 		if (root == null) {
 			return false;
 		} else {
@@ -167,7 +171,7 @@ public class KDTree {
 		return false;
 	}
 	
-    public static void inOrder(Node root){ 
+    public static void inOrder(NodeK root){ 
         if (root == null) {
             return;
         }
@@ -178,9 +182,9 @@ public class KDTree {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		Node root = null;
+		NodeK root = null;
 		int size= 36650;      
-		while(size>0) {             //不知道为什么如果用in.nextline()的话永远都不会break。。。
+		while(size>0) {             //in.nextline() will never break
 			String tmp = in.nextLine();
 			String[] tmp1 = tmp.split(",");
 			double la = Double.valueOf(tmp1[0]);
@@ -189,10 +193,17 @@ public class KDTree {
 			size--;
 		}
 		in.close();
-		Boolean res = efind(root, 46.6436,-118.5566);  //return true
-		System.out.println(res);
-		Node[] range = find(root, 46.6436,-118.5566,4);
-		//System.out.println(root.getLeft().getX());
-		//inOrder(root);
+		Boolean ex1 = efind(root, 46.6436, -118.5566); //return true
+		System.out.println("City (46.6436, -118.5566) has been found:");
+		System.out.println(ex1);
+		Boolean ex2 = efind(root, 46, -118);//return false
+		System.out.println("City (46.6436, -118.5566) has been found:");
+		System.out.println(ex2);
+		find(root, 46.6436,-118.5566,1);
+		System.out.println();
+		System.out.println("There are "+arr.size()+" citizes in the circle:");
+		for(NodeK node: arr) {
+			System.out.println(node);
+		}
 	}
 }
